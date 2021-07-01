92fc6113639aa028d1b90cb53e32b6e34e1f1f27
Xu_Guo <guo12@iu.edu>
2021-07-01 01:50:09 -0400

    rename vars and funcs

--- mmic_md/components/sample_component.py
+++ mmic_md/components/sample_component.py
@@ -7,37 +7,30 @@ from typing import List, Tuple, Optional
 # Import the generic i.e. starting component from MMIC
 from mmic.components.blueprints.generic_component import GenericComponent
 
-# Import common functions from MMElemental
-from mmelemental.util.decorators import require
-from mmelemental.util.units import convert
-
-# Import MMSchema models from MMElemental
-from mmelemental.models.molecule import Molecule
-from mmelemental.models.forcefield import ForceField
-
-__all__ = ["SampleComponent"]
+from ..models.input import MDInput
+from ..models.output import MDOutput
 
 
-class SampleComponent(GenericComponent):
-    """ A sample component that reads in a Molecule and returns a ForceField object. """
+__all__ = ["MDComponent"]
 
-    @classmethod
-    def input(cls):
-        return Molecule
 
-    @classmethod
-    def output(cls):
-        return ForceField
+class MDComponent(GenericComponent):
+	""" A sample component that reads in a Molecule and returns a ForceField object. """
 
-    @require("cookiecutter.code_name")
-    def execute(
-        self,
-        inputs: Molecule,
-        extra_outfiles: Optional[List[str]] = None,
-        extra_commands: Optional[List[str]] = None,
-        scratch_name: Optional[str] = None,
-        timeout: Optional[int] = None,
-    ) -> Tuple[bool, ForceField]:
-        import cookiecutter.code_name
+	@classmethod
+	def input(cls):
+		return MDInput
 
-        return True, ForceField(**kwargs)
+	@classmethod
+	def output(cls):
+		return MDOutput
+
+
+	@property
+	def supported_comps(self) -> Set[str]:
+		"""Returns the supported components e.g. set(['mmic_mda',...]).
+		Returns
+		-------
+		Set[str]
+		"""
+		return set(["mmic_md_openmm","mmic_md_gmx"])
