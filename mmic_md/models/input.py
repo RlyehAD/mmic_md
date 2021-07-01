ed0a0614fd07deb4ec04818e5c8487ffa68348f2
Xu_Guo <guo12@iu.edu>
2021-07-01 01:43:34 -0400

    Add vars to input/output model

---
+++ mmic_md/models/input.py
@@ -0,0 +1,104 @@
+from mmelemental.models.proc.base import ProcInput
+from mmelemental.models import Molecule, ForceField, ForceInput  # TrajInput
+from pydantic import Field, validator
+from typing import Optional, Dict, List, Tuple
+
+__all__ = ["MD_Input"]
+
+
+class MDInput(ProcInput):
+    """Basic input model for MD run."""
+
+    # System fields
+    molecule: Dict[str, Molecule] = Field(
+        ...,
+        description="Molecular mechanics molecule object(s). See the :class:``Molecule`` class. "
+        "Example: mol = {'ligand': Molecule, 'receptor': Molecule, 'solvent': Molecule}.",
+    )
+    forcefield: Dict[str, ForceField] = Field(
+        ...,
+        description='Forcefield object(s) or name(s) for every Molecule defined in "mol".',
+    )
+    cell: Optional[Tuple[Tuple[float], Tuple[float]]] = Field(
+        None,
+        description="Cell dimensions in the form: ((xmin, ymin, ...), (xmax, ymax, ...))",
+    )
+    boundary: List[str] = Field(
+        None,
+        description="Boundary conditions in all dimensions e.g. (periodic, periodic, periodic) imposes periodic boundaries in 3D.",
+    )
+
+    # I/O fields
+    trajectory: Optional[Dict[str, TrajInput]] = Field(
+        None,
+        description="Trajectories to write for quantity 'key' every 'value' steps. e.g. {'geometry_freq': 10, 'velocities_freq': 100, 'forces_freq': 50} "
+        "produces 3 trajectory objects storing positions every 10 steps, velocities, every 100 steps, and forces every 50 steps.",
+    )
+
+    # Global fields
+    max_steps: int = Field(
+        None, description="Max number of optimization steps to perform."
+    )
+    step_size: float = Field(None, description="Step size for constant step marching.")
+    step_size_units: Optional[str] = Field(
+        None,
+        description="Step size unit. Any unit supported by pint is allowed. If not defined, it's set to fs",
+    )
+
+    # Algorithmic fields
+    method: str = Field(
+        None,
+        description="The integrator used for MD simulation. e.g. 'leapfrog'",
+    )
+
+    # Output control
+    F_xout: float = Field(
+        None,
+        description="The frequency for the simulation to write a frame of coordinates",
+    )
+    F_vout: float = Field(
+        None,
+        description="The frequency for the simulation to write the velocities",
+    )
+    F_eout: float = Field(
+        None,
+        description="The frequency for the simulation to write the energies",
+    )
+    # A F for log file output?
+    F_stdout: float = Field(
+        None,
+        description="The frequecy for writing an standard output including info of T, P, E, and so on",
+    )
+    F_unit: str = Field(
+        None,
+        description="The unit for the frequencies to write coordinates, velocities, and energies",
+    )  # May be deleted
+
+    # Bonded interaction
+    unconstrained_start: str = Field(
+        None,
+        description="To determine if constraints are applied to the initial configuration. It's no by default",
+    )  # May be deleted
+    constraint_method: str = Field(
+        None,
+        description="The algorithm used for constranits. e.g. 'SHAKE'",
+    )
+    constraints: str = Field(
+        None,
+        description="The bonds that are constrained",
+    )  # May be replaced by bond_const
+
+    # Nonbonded interaction
+    short_forces: ForcesInput = Field(
+        ..., description="Algorithms for computing short-range forces."
+    )  # Is choosing something like Buckingham potential possible? I did not find related vars in gmx or openmm. It should be
+    # included in the ff
+
+    long_forces: ForcesInput = Field(
+        ..., description="Algorithms for computing long-range forces."
+    )
+    cut_off: str = Field(None, description="Neighbor searching algorithm")
+
+    # Temperature and pressure coupling
+    t_couple: str = Field(..., description="Temperature coupling algorithm")
+    p_couple: str = Field(..., description="Pressure coupling algorithm")
---
+++ mmic_md/models/output.py
@@ -0,0 +1,28 @@
+from mmelemental.models.proc.base import ProcOutput
+from .input import MDInput
+from mmelemental.models import Molecule, Ensemble, Trajectory
+from pydantic import Field
+from typing import Optional, Dict, List
+
+__all__ = ["MDOutput"]
+
+
+class MDOutput(ProcOutput):
+    proc_input: OptimInput = Field(
+        ..., description="Input schema used to run optimization"
+    )
+    molecule: Dict[str, Molecule] = Field(
+        ...,
+        description="Molecular mechanics molecule object(s). See the :class:``Molecule`` class. "
+        "Example: mol = {'ligand': Molecule, 'receptor': Molecule, 'solvent': Molecule}.",
+    )
+    ensemble: Optional[Dict[str, Ensemble]] = Field(
+        None,
+        description="Ensemble output for a series of microstates of molecules. "
+        "See the :class:``Ensemble`` class.",
+    )
+    trajectory: Optional[Dict[str, Trajectory]] = Field(
+        None,
+        description="Trajectory output representing a series of snapshots of the system at "
+        "different timesteps. See the :class:``Trajectory`` class.",
+    )
